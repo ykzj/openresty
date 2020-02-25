aee8f33025f5b41ec956048b29cc511adc74b774
Xudong Liu <lxd@lxd-macbookpro.roam.corp.google.com>
2020-02-25 22:14:05 +0800

    modify run2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL_LIST="https://cdn.gcpce.com/8MB https://googlecdn.apcdns.com/8MB"
+URL_LIST="https://cdn.gcpce.com/8MB https://googlecdn.apcdns.com/8MB https://tx.gcpce.com/8MB"
 while :
 do
     for url in $URL_LIST
