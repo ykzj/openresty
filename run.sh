8ff2436300097828ac0af79546cae6b144cf7400
Xudong Liu <lxd@lxd-macbookpro.roam.corp.google.com>
2020-02-25 21:18:11 +0800

    add run

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+URL_LIST="https://tx.gcpce.com/8MB https://googlecdn.apcdns.com/8MB"
+while :
+do
+    for url in $URL_LIST
+    do
+        curl -qo /dev/null $url
+        sleep 60
+    done
+    sleep 300
+done
